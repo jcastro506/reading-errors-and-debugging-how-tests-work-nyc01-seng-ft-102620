4de1a849ee2c74ef75315ba7164c7dcd38f2c1a0
breezy-builder-9254 <castrojoshua805@gmail.com>
2020-09-05 00:39:33 +0000

    Done.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,5 +1,18 @@
 # Code your solution in calculator.rb
 
+first_number = 5
+
+second_number = 2 
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
+
+
 describe "./calculator.rb" do
 
   it "contains a local variable called first_number that is assigned to a number" do
