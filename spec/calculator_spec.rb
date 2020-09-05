fc8451f154987c7c34fcf0fddda21536ec62e068
breezy-builder-9254 <castrojoshua805@gmail.com>
2020-09-05 00:20:32 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,5 +1,12 @@
 # Code your solution in calculator.rb
 
+first_number = 5
+
+second_number = 2 
+
+sum = first_number + second_number
+
+
 describe "./calculator.rb" do
 
   it "contains a local variable called first_number that is assigned to a number" do
